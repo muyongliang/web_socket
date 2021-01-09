d739ad4cb480a487c5cd3b1354cd6dc39eb347be
muyongliang <m18161254878@aliyun.com>
2021-01-09 15:42:10 +0800

    初始化

---
+++ src/main/java/com/study/WebSocketApplication.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author muyongliang
+ * @Date 2020/12/21
+ * @Description com.study.WebSocketApplication
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/study/controller/SocketController.java
@@ -0,0 +1,28 @@
+package com.study.controller;
+
+import com.study.service.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SocketController {
+
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/webSocket")
+    public ModelAndView socket() {
+        ModelAndView mav = new ModelAndView("/webSocket");
+        return mav;
+    }
+
+
+}
